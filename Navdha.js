8a63b6f9f97a34850c3330ec541e2d2ffa280957
animals-ai <62425557+animals-ai@users.noreply.github.com>
2020-05-26 05:17:06 -0700

    Add files via upload

---
+++ Navdha.js
@@ -0,0 +1,12 @@
+function displayDate(){
+	document.getElementById("date").innerHTML=Date();
+}
+	
+	welcomeMsg(x);
+	var firstname=welcomeMsg;
+	function welcomeMsg(msg){
+		alert(firstname);
+		
+	}
+		
+	
